693565f8876f1bc5181bd717415acbb882f4e45e
paul-brabet <pbrabet@gmail.com>
2017-07-26 17:35:28 +1200

    preliminary oauth.io

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 var server = require('./server')
+require('dotenv').config()
 
 var PORT = process.env.PORT || 3000
 
-server.listen(PORT, function () {
-  console.log('Listening on port', PORT)
+server.listen(PORT, function() {
+    console.log('Listening on port', PORT)
 })
